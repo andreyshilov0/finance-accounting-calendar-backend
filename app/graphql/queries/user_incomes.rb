43322e58097e45e7e66701720683dffaba97eb64
andreyshilov0 <an.schilow@yandex.ru>
2023-10-30 16:37:41 +0300

    add filter financials

--- app/graphql/queries/user_incomes.rb
+++ app/graphql/queries/user_incomes.rb
@@ -2,8 +2,15 @@ module Queries
   class UserIncomes < Queries::BaseQuery
     type [Types::IncomeType], null: true
 
-    def resolve
-      current_user.incomes || (raise execution_error(I18n.t('failed_to_fetch_user_incomes')))
+    argument :page, Integer, required: false, default_value: 1
+    argument :per_page, Integer, required: false, default_value: 7
+    argument :month_year, String, required: true
+
+    def resolve(page:, per_page:, month_year:)
+      start_date = Date.strptime(month_year, '%Y-%m')
+      end_date = start_date.end_of_month
+      incomes = current_user.incomes.where(date: start_date..end_date).order(created_at: :desc).page(page).per(per_page)
+      incomes.present? ? incomes : (raise execution_error(I18n.t('failed_to_fetch_user_incomes')))
     end
   end
 end
--- app/graphql/queries/user_payments.rb
+++ app/graphql/queries/user_payments.rb
@@ -2,8 +2,15 @@ module Queries
   class UserPayments < Queries::BaseQuery
     type [Types::PaymentType], null: true
 
-    def resolve
-      current_user.payments || (raise execution_error(I18n.t('failed_to_fetch_user_payments')))
+    argument :page, Integer, required: false, default_value: 1
+    argument :per_page, Integer, required: false, default_value: 7
+    argument :month_year, String, required: true
+
+    def resolve(page:, per_page:, month_year:)
+      start_date = Date.strptime(month_year, '%Y-%m')
+      end_date = start_date.end_of_month
+      payments = current_user.payments.where(date: start_date..end_date).order(created_at: :desc).page(page).per(per_page)
+      payments.present? ? payments : (raise execution_error(I18n.t('failed_to_fetch_user_payments')))
     end
   end
 end
